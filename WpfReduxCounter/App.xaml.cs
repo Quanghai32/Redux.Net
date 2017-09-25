f2fd9522f006e2e25196074bbfaf610c446ad8e4
Tran Duc Hai <quanghai.dt4@hotmail.com>
2017-09-25 23:50:32 +0700

    Revert "Revert "Add WPF project""
    This reverts commit 8cdd1338fc9e564e62797019792a3555f8cbc3a6.

---
+++ WpfReduxCounter/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Redux;
+
+namespace WpfReduxCounter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IStore<int> CounterStore { get; private set; }
+        public App()
+        {
+            CounterStore = new Store<int>(CounterReducer.Execute, 0);
+        }
+    }
+}
